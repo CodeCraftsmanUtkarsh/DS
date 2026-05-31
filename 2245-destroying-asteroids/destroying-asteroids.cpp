0bdac41b35031d66e6dea1fbd9c0e99e7b4a24c6
CodeCraftsmanUtkarsh <utkarshsrivastava.1392006@gmail.com>
2026-05-31 10:15:11 +0530

    Time: 36 ms (37.08%) | Memory: 106.8 MB (48.03%) - LeetSync

--- 2245-destroying-asteroids/destroying-asteroids.cpp
+++ 2245-destroying-asteroids/destroying-asteroids.cpp
@@ -1,66 +1,17 @@
 class Solution {
 public:
-
-    void merge(vector<int>& a, int l, int mid, int r) {
-
-        int n = mid - l + 1;
-        int m = r - mid;
-
-        vector<int> left(n);
-        vector<int> right(m);
-
-        for(int i=0;i<n;i++)
-            left[i] = a[l+i];
-
-        for(int j=0;j<m;j++)
-            right[j] = a[mid+1+j];
-
-        int i=0, j=0;
-        int k=l;
-
-        while(i<n && j<m){
-            if(left[i] <= right[j]){
-                a[k++] = left[i++];
-            }
-            else{
-                a[k++] = right[j++];
-            }
-        }
-
-        while(i<n)
-            a[k++] = left[i++];
-
-        while(j<m)
-            a[k++] = right[j++];
-    }
-
-    void mergeSort(vector<int>& a, int l, int r){
-
-        if(l >= r)
-            return;
-
-        int mid = (l+r)/2;
-
-        mergeSort(a,l,mid);
-        mergeSort(a,mid+1,r);
-
-        merge(a,l,mid,r);
-    }
-
     bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
-
-        mergeSort(asteroids,0,asteroids.size()-1);
-
-        long long curr = mass;
-
-        for(int x : asteroids){
-
-            if(x > curr)
+        sort(asteroids.begin(),
+             asteroids.end());  // Sort by mass in ascending order
+        long long mass1 = mass;
+        for (const int asteroid : asteroids) {
+            // Traverse the asteroids in order, attempt to destroy and update
+            // mass or return the result
+            if (mass1 < asteroid) {
                 return false;
-
-            curr += x;
+            }
+            mass1 += asteroid;
         }
-
-        return true;
+        return true;  // Successfully destroy all asteroids
     }
 };
